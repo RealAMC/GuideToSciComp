3073f9be1ae0c8171dfbaedf369f668cb2c7c57b
RealAMC <AdrianMCook@protonmail.com>
2020-02-09 14:17:06 +1300

    Added Ex 1.5

---
+++ Chapter 1 Getting Started/Ex_1_5.cpp
@@ -0,0 +1,26 @@
+// Ex 1.5
+/*
+Write code that invites the user to input separately strings that store their given name and their family name. Print the user's full name to screen.
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main (int argc, char* argv[])
+{
+	std::string Fname;
+	std::string Lname;
+	
+	// Get Firstname
+	std::cout << "Enter your first name and then hit RETURN\n";
+	std::getline(std::cin, Fname);
+	
+	// Get Lastname
+	std::cout << "Enter your last name and then hit RETURN\n";
+	std::getline(std::cin, Lname);
+	
+	// Print full name
+	std::cout << "Hello, " << Fname << " " << Lname;
+	
+	return 0;
+}
